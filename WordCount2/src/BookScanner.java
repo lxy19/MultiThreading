9f9e2a4a88e78091a66f97a2ac55802eb105226d
Xiangye Li <xiangyeli@gmail.com>
2019-11-17 11:31:18 -0500

    Add WordCount

---
+++ WordCount2/src/BookScanner.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.atomic.LongAdder;
+
+public class BookScanner implements Runnable{
+
+    private Map<String, LongAdder> counter;
+    private String pathToTextFile;
+    private String bookName;
+    private Scanner bookScanner;
+
+    public BookScanner(String pathToTextFile, Map<String, LongAdder> countMap){
+        this.pathToTextFile = pathToTextFile;
+        this.counter = countMap;
+        this.bookName = pathToTextFile.substring(pathToTextFile.lastIndexOf('/')+1);
+        try {
+            bookScanner = new Scanner(new File(pathToTextFile));
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        //Step:
+        // scan each line
+        //
+        while(bookScanner.hasNext()){
+               String line = bookScanner.nextLine();
+               String[]words = line.split("\\s");
+               for(String word: words) {
+                   counter.computeIfAbsent(word, c->new LongAdder()).increment();
+               }
+        }
+    }
+}
